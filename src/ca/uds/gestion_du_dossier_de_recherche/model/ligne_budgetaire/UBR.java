542c15f505cbfd83446a2966a84a19d4d98b3653
ValentinLH <98545666+ValentinLH@users.noreply.github.com>
2024-03-22 19:20:46 -0400

    ajout contrainte / dateDébut / dateFin ubr

--- src/ca/uds/gestion_du_dossier_de_recherche/model/ligne_budgetaire/UBR.java
+++ src/ca/uds/gestion_du_dossier_de_recherche/model/ligne_budgetaire/UBR.java
@@ -1,5 +1,6 @@
 package ca.uds.gestion_du_dossier_de_recherche.model.ligne_budgetaire;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,12 +10,18 @@ public class UBR {
 	private Organisme organisme;
 	private int code;
 	private Map<LigneBudgetaire, Float> montantsLignesBudgetaire;
+	private boolean contraintes;
+	private LocalDate dateDebut;
+	private LocalDate dateFin;
 
 	public UBR() {
 		super();
 		this.organisme = null;
 		this.code = 0;
 		this.montantsLignesBudgetaire = new HashMap<>();
+        this.contraintes = false;
+        this.dateDebut = null;
+        this.dateFin = null;
 	}
 
 	public UBR(Organisme organisme, int code) {
@@ -22,6 +29,9 @@ public class UBR {
 		this.organisme = organisme;
 		this.code = code;
 		this.montantsLignesBudgetaire = new HashMap<>();
+        this.contraintes = false;
+        this.dateDebut = null;
+        this.dateFin = null;
 	}
 
 	public UBR(Organisme organisme, int code, Map<LigneBudgetaire, Float> montantsLignesBudgetaire) {
@@ -29,6 +39,33 @@ public class UBR {
 		this.organisme = organisme;
 		this.code = code;
 		this.montantsLignesBudgetaire = montantsLignesBudgetaire;
+        this.contraintes = false;
+        this.dateDebut = null;
+        this.dateFin = null;
+	}
+
+	
+	
+	public UBR(Organisme organisme, int code, Map<LigneBudgetaire, Float> montantsLignesBudgetaire,
+			boolean contraintes) {
+		super();
+		this.organisme = organisme;
+		this.code = code;
+		this.montantsLignesBudgetaire = montantsLignesBudgetaire;
+		this.contraintes = contraintes;
+		this.dateDebut = null;
+        this.dateFin = null;
+	}
+
+	public UBR(Organisme organisme, int code, Map<LigneBudgetaire, Float> montantsLignesBudgetaire, boolean contraintes,
+			LocalDate dateDebut, LocalDate dateFin) {
+		super();
+		this.organisme = organisme;
+		this.code = code;
+		this.montantsLignesBudgetaire = montantsLignesBudgetaire;
+		this.contraintes = contraintes;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
 	}
 
 	/**
@@ -81,7 +118,7 @@ public class UBR {
 	public void ajouterLigneBudgetaire(LigneBudgetaire ligneBudgetaire, float montant) {
 		montantsLignesBudgetaire.put(ligneBudgetaire, montant);
 		if (!ligneBudgetaire.getUbrs().contains(this)) {
-			ligneBudgetaire.ajouterUBR(this,montant);
+			ligneBudgetaire.ajouterUBR(this, montant);
 		}
 	}
 
@@ -91,7 +128,7 @@ public class UBR {
 		if (!ligneBudgetaire.getUbrs().contains(this)) {
 			ligneBudgetaire.supprimerUBR(this);
 		}
-		
+
 	}
 
 	@Override
@@ -115,5 +152,37 @@ public class UBR {
 		sb.append("}]");
 		return sb.toString();
 	}
+
+	public boolean isContraintes() {
+		return contraintes;
+	}
+
+	public void setContraintes(boolean contraintes) {
+		this.contraintes = contraintes;
+	}
+
+	   public LocalDate getDateDebut() {
+	        return dateDebut;
+	    }
+
+	    public void setDateDebut(LocalDate dateDebut) {
+	        this.dateDebut = dateDebut;
+	        // Vérifie si la date de fin est définie et si elle est inférieure à la date de début
+	        if (dateFin != null && dateFin.isBefore(dateDebut)) {
+	            this.dateFin = dateDebut;
+	        }
+	    }
+
+	    public LocalDate getDateFin() {
+	        return dateFin;
+	    }
+
+	    public void setDateFin(LocalDate dateFin) {
+	        // Vérifie si la date de fin est inférieure à la date de début
+	        if (dateDebut != null && dateFin.isBefore(dateDebut)) {
+	            throw new IllegalArgumentException("La date de fin doit être supérieure ou égale à la date de début.");
+	        }
+	        this.dateFin = dateFin;
+	    }
 
 }
